710083a981903d4053c72a1cd022ed08b82564db
André Reitan <andre@reitans.org>
2025-08-08 11:00:53 +0200

    HashRouter for å fikse refreshfeil i GitHub

--- Frontend/src/app.jsx
+++ Frontend/src/app.jsx
@@ -1,24 +1,21 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Menu from './Menu';
 import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
-    <Router basename="/nfreg3">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/menu"
-          element={
-            <ProtectedRoute>
-              <Menu />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route
+        path="/menu"
+        element={
+          <ProtectedRoute>
+            <Menu />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
 
