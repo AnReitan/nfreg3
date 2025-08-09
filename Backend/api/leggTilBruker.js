ef49fbcd0ed45be9ceb230f0e5a9605d46cd525d
André Reitan <andre@reitans.org>
2025-08-09 13:07:49 +0200

    oppdatert

--- Backend/api/leggTilBruker.js
+++ Backend/api/leggTilBruker.js
@@ -52,7 +52,7 @@ export default async function handler(req, res) {
 
     // Kjør SQL-insert
     await db.query(
-      `INSERT INTO b_users
+      `INSERT INTO d_users
         (s_name, s_email, s_pwd, s_regno, i_userlevel, b_active, dt_modify)
        VALUES (?, ?, ?, ?, ?, ?, ?)`,
       [
